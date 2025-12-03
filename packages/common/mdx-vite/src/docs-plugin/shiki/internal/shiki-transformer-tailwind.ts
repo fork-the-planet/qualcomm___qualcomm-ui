bd11b84aaf9dbd080fa999f03f809ea1855b65d0
Ryan Bower <rbower@qti.qualcomm.com>
2025-12-02 23:27:35 -0800

    add license headers
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/mdx-vite/src/docs-plugin/shiki/internal/shiki-transformer-tailwind.ts
+++ packages/common/mdx-vite/src/docs-plugin/shiki/internal/shiki-transformer-tailwind.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import type {Element, Root, Text} from "hast"
 import {fromHtml} from "hast-util-from-html"
 import {toHtml} from "hast-util-to-html"
