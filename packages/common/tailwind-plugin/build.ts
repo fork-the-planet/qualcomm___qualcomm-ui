6da2dc08b422d437e69efddc14b1e716e4ecb316
Ryan Bower <rbower@qti.qualcomm.com>
2025-12-04 21:11:32 -0800

    wip theme docs
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/tailwind-plugin/build.ts
+++ packages/common/tailwind-plugin/build.ts
@@ -1,17 +1,38 @@
 import type {BuildOptions} from "esbuild"
 import {writeFile} from "node:fs/promises"
 
-import {buildOrWatch, hasArg, logPlugin} from "@qualcomm-ui/esbuild"
+import {
+  buildOrWatch,
+  collectFolders,
+  hasArg,
+  logPlugin,
+} from "@qualcomm-ui/esbuild"
 
 import pkg from "./package.json"
+
+async function collectEntryPoints() {
+  // Create an object with output names as keys and entry points as values
+  return (await collectFolders("./src")).reduce(
+    (acc: Record<string, string>, name) => {
+      acc[`${name}/index`] = `./src/${name}/index.ts`
+      return acc
+    },
+    {},
+  )
+}
 
 async function main(argv: string[]) {
   const IS_WATCH = hasArg(argv, "--watch")
 
   const buildOpts: BuildOptions = {
     bundle: true,
-    external: [...Object.keys(pkg.peerDependencies)],
+    entryPoints: await collectEntryPoints(),
+    external: [
+      ...Object.keys(pkg.devDependencies),
+      ...Object.keys(pkg.peerDependencies),
+    ],
     metafile: true,
+    outdir: "dist",
     platform: "node",
     plugins: [logPlugin({bundleSizeOptions: {logMode: "both"}})],
     sourcemap: true,
@@ -19,33 +40,29 @@ async function main(argv: string[]) {
     tsconfig: "tsconfig.lib.json",
   }
 
-  await Promise.all([
-    buildOrWatch(
-      {
-        ...buildOpts,
-        entryPoints: ["./src/vscode/index.ts"],
-        format: "esm",
-        logLevel: IS_WATCH ? "error" : "warning",
-        outfile: "dist/vscode/index.js",
-        plugins: [
-          logPlugin({bundleSizeOptions: {logMode: "both"}}),
-          {
-            name: "generate metafile",
-            setup(build) {
-              build.onEnd(async (res) => {
-                await writeFile(
-                  "build-metafile.json",
-                  JSON.stringify(res.metafile),
-                  "utf-8",
-                )
-              })
-            },
+  await buildOrWatch(
+    {
+      ...buildOpts,
+      format: "esm",
+      logLevel: IS_WATCH ? "error" : "warning",
+      plugins: [
+        logPlugin({bundleSizeOptions: {logMode: "both"}}),
+        {
+          name: "generate metafile",
+          setup(build) {
+            build.onEnd(async (res) => {
+              await writeFile(
+                "build-metafile.json",
+                JSON.stringify(res.metafile),
+                "utf-8",
+              )
+            })
           },
-        ],
-      },
-      IS_WATCH,
-    ),
-  ])
+        },
+      ],
+    },
+    IS_WATCH,
+  )
 }
 
 main(process.argv)
