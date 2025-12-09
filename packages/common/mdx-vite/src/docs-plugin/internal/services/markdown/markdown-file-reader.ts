57aa708ffed2c1dc2412bd0c3c74efdda5604c22
Ryan Bower <rbower@qti.qualcomm.com>
2025-12-08 19:42:43 -0800

    debug jenkins issues
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/mdx-vite/src/docs-plugin/internal/services/markdown/markdown-file-reader.ts
+++ packages/common/mdx-vite/src/docs-plugin/internal/services/markdown/markdown-file-reader.ts
@@ -48,6 +48,7 @@ export function getGitMetadata(
     const repoRoot = getRepoRoot()
     const relativePath = relative(repoRoot, filePath)
     const format = mode === "user-and-timestamp" ? "%cI%n%aN" : "%cI"
+    console.debug({filePath, relativePath, repoRoot})
     const result = execSync(
       `git log -1 --format=${format} -- "${relativePath}"`,
       {
