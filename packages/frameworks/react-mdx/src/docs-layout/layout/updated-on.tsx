7c505126cd61a75da77849261e741b302671bc8b
Ryan Bower <rbower@qti.qualcomm.com>
2025-12-08 17:19:59 -0800

    fix license check
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/react-mdx/src/docs-layout/layout/updated-on.tsx
+++ packages/frameworks/react-mdx/src/docs-layout/layout/updated-on.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import type {ComponentPropsWithoutRef, ReactElement} from "react"
 
 import {mergeProps} from "@qualcomm-ui/utils/merge-props"
