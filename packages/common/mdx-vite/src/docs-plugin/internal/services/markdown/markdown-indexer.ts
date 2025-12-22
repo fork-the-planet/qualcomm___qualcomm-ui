a74942199ee83d38875ad710e5fcbffdb6b655e7
Ryan Bower <rbower@qti.qualcomm.com>
2025-12-22 01:05:19 -0800

    markdown docs updated, lint
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/mdx-vite/src/docs-plugin/internal/services/markdown/markdown-indexer.ts
+++ packages/common/mdx-vite/src/docs-plugin/internal/services/markdown/markdown-indexer.ts
@@ -20,10 +20,6 @@ import type {
 } from "@qualcomm-ui/mdx-common"
 
 import {rehypeSlug} from "../../../rehype/rehype-slug"
-import {
-  remarkFrontmatterDescription,
-  remarkFrontmatterTitle,
-} from "../../../remark"
 import {remarkAlerts} from "../../../remark/remark-alerts"
 import {remarkFrontmatterInterpolation} from "../../../remark/remark-frontmatter-interpolation"
 
