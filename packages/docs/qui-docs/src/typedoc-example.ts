3d4621f461c84443e2acafe0111b0424e4a69db0
Ryan Bower <rbower@qti.qualcomm.com>
2026-01-02 19:46:14 -0800

    fix(typedoc): improve method arg styling
    Add typedoc class example to debug docs
    
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/docs/qui-docs/src/typedoc-example.ts
+++ packages/docs/qui-docs/src/typedoc-example.ts
@@ -123,3 +123,95 @@ export interface SinceExample {
    */
   foo: string
 }
+
+export type DocsEnvironment = "test" | "stage" | "prod"
+
+/**
+ * @public
+ */
+export interface ZipFileOptions {
+  /**
+   * name to use for the local zip file.
+   *
+   * @default `site-data.zip`
+   */
+  archiveName?: string
+
+  /**
+   * the file directory to zip up.
+   */
+  directory: string
+}
+
+/**
+ * @public
+ */
+export interface UploadFileOptions {
+  /**
+   * Relative path to the local zip file that will be generated.
+   */
+  archivePath: string
+
+  /**
+   * Deploy environment.
+   */
+  environment: DocsEnvironment
+
+  /**
+   * Name of the service. Must match one of the predefined service names in the
+   * deployed handler.
+   */
+  service: string
+}
+
+/**
+ * @public
+ */
+export interface GetFilesOptions {
+  /**
+   * Deploy environment.
+   */
+  environment: DocsEnvironment
+
+  /**
+   * Name of the service. Must match one of the predefined service names in the
+   * deployed handler.
+   */
+  service: string
+}
+
+export interface CloudDocsSdkOptions {
+  /**
+   * The file handler API url.
+   */
+  baseUrl: string
+}
+
+export class CloudDocsSdk {
+  constructor(public readonly opts: CloudDocsSdkOptions) {}
+
+  /**
+   * Zip files.
+   */
+  async zipFiles(_opts: ZipFileOptions): Promise<boolean> {
+    return false
+  }
+
+  /**
+   * Upload files
+   *
+   * @param archivePath test1
+   * @param environment test2
+   * @param service test3
+   */
+  async uploadFiles(_opts: UploadFileOptions): Promise<boolean> {
+    return true
+  }
+
+  /**
+   * Download files
+   */
+  async getServiceFiles(_opts: GetFilesOptions) {
+    return {} as any
+  }
+}
