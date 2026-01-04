2b605787f8966de8fde0bfb552eebdb610262034
Ryan Bower <rbower@qti.qualcomm.com>
2026-01-03 21:19:45 -0800

    add react code connect config
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

---
+++ packages/frameworks/react/src/button/figma/button.figma.tsx
@@ -0,0 +1,65 @@
+import figma from "@figma/code-connect"
+
+import {Button} from "@qualcomm-ui/react/button"
+
+const sharedProps = {
+  disabled: figma.enum("state", {
+    disabled: true,
+  }),
+  emphasis: figma.enum("emphasis", {
+    "black-persistent": "black-persistent",
+    danger: "danger",
+    neutral: "neutral",
+    primary: "primary",
+    "white-persistent": "white-persistent",
+  }),
+  variant: figma.enum("variant", {
+    filled: "fill",
+    ghost: "ghost",
+    outline: "outline",
+  }),
+}
+
+const sizeProp = {
+  size: figma.enum("size", {
+    lg: "lg",
+    md: "md",
+    sm: "sm",
+  }),
+}
+
+// TODO: solve these issues: https://qualcomm-confluence.atlassian.net/wiki/spaces/SAGAUX/pages/2869087007/QDS+Figma+Code+Connect+discussion
+figma.connect(
+  Button,
+  "https://www.figma.com/design/ETvFgN3bbNvr6sbpoZyNuA/Base-Component-Library-v1.0.4?node-id=3571%3A1400",
+  {
+    example: (props) => {
+      return (
+        <Button
+          disabled={props.disabled}
+          emphasis={props.emphasis}
+          size={props.nested.size}
+          variant={props.variant}
+        >
+          {props.nested.label}
+        </Button>
+      )
+    },
+    props: {
+      ...sharedProps,
+      nested: figma.nestedProps("_Button Foundation", {
+        ...sizeProp,
+        icon: figma.enum("icon", {
+          end: "end",
+          none: "none",
+          only: "only",
+          start: "start",
+        }),
+        iconLg: figma.string("iconLg"),
+        iconMd: figma.string("iconMd"),
+        iconSm: figma.string("iconSm"),
+        label: figma.string("label"),
+      }),
+    },
+  },
+)
