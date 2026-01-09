41248cb38dec3d13c6745e8f5f6b623183caa860
Olaf Kappes <okappes@qti.qualcomm.com>
2026-01-09 10:15:39 +0100

    feat: text area
    Signed-off-by: Olaf Kappes <okappes@qti.qualcomm.com>

---
+++ packages/frameworks/react/src/text-area/figma/text-area.figma.tsx
@@ -0,0 +1,43 @@
+import figma from "@figma/code-connect"
+
+import {TextArea} from "@qualcomm-ui/react/text-area"
+
+const sharedProps = {
+  counter: figma.boolean("count"),
+  defaultValue: figma.boolean("filled", {
+    true: figma.string("inputText"),
+  }),
+  disabled: figma.enum("state", {
+    disabled: true,
+  }),
+  errorText: figma.string("errorText"),
+  hint: figma.boolean("hint", {
+    true: figma.string("hintText"),
+  }),
+  invalid: figma.enum("state", {
+    invalid: true,
+    "invalid-focus": true,
+  }),
+  label: figma.boolean("label", {
+    true: figma.string("labelText"),
+  }),
+  maxLength: figma.boolean("count", {
+    true: 100,
+  }),
+  placeholder: figma.string("holderText"),
+  readOnly: figma.enum("state", {
+    "read-only": true,
+  }),
+  required: figma.boolean("required"),
+  size: figma.enum("size", {
+    lg: "lg",
+    sm: "sm",
+  }),
+}
+
+figma.connect(TextArea, "<FIGMA_COMPONENTS_BASE>?node-id=4587-9674", {
+  example: ({hint, label, ...props}) => (
+    <TextArea hint={hint} label={label} {...props} />
+  ),
+  props: sharedProps,
+})
