1d02314afc1dfaa334d4713eeb29da9fdda80713
Olaf Kappes <okappes@qti.qualcomm.com>
2026-01-09 09:45:09 +0100

    feat: switch
    Signed-off-by: Olaf Kappes <okappes@qti.qualcomm.com>

---
+++ packages/frameworks/react/src/switch/figma/switch.figma.tsx
@@ -0,0 +1,38 @@
+import figma from "@figma/code-connect"
+
+import {Switch} from "@qualcomm-ui/react/switch"
+
+const sharedProps = {
+  defaultChecked: figma.enum("variant", {
+    checked: true,
+    "invalid-checked": true,
+  }),
+  disabled: figma.enum("state", {
+    disabled: true,
+  }),
+  invalid: figma.enum("variant", {
+    "invalid-checked": true,
+    "invalid-unchecked": true,
+  }),
+  size: figma.enum("size", {
+    sm: "sm",
+  }),
+}
+
+// Basic Switch (no label)
+figma.connect(Switch, "<FIGMA_COMPONENTS_BASE>?node-id=12609-520", {
+  example: (props) => <Switch {...props} />,
+  props: sharedProps,
+})
+
+// Switch with label
+figma.connect(Switch, "<FIGMA_COMPONENTS_BASE>?node-id=2270-5170", {
+  example: ({label, ...props}) => <Switch label={label} {...props} />,
+  props: {
+    ...sharedProps,
+    errorText: figma.string("errorText"),
+    label: figma.boolean("label", {
+      true: figma.string("labelText"),
+    }),
+  },
+})
