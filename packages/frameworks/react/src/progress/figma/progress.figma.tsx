9ba54c158fb34a304b7a7e80b8de36920065bd44
Olaf Kappes <okappes@qti.qualcomm.com>
2026-01-13 11:19:59 +0100

    feat: progress (bar)
    Signed-off-by: Olaf Kappes <okappes@qti.qualcomm.com>

---
+++ packages/frameworks/react/src/progress/figma/progress.figma.tsx
@@ -0,0 +1,50 @@
+import figma from "@figma/code-connect"
+
+import type {
+  QdsProgressEmphasis,
+  QdsProgressLabelOrientation,
+  QdsProgressSize,
+} from "@qualcomm-ui/qds-core/progress"
+import {Progress} from "@qualcomm-ui/react/progress"
+
+const sharedProps = {
+  disabled: figma.enum("state", {
+    disabled: true,
+  }),
+  emphasis: figma.enum<QdsProgressEmphasis>("emphasis", {
+    neutral: "neutral",
+  }),
+  errorText: figma.string("errorText"),
+  hint: figma.string("hintText"),
+  invalid: figma.enum("state", {
+    invalid: true,
+  }),
+  label: figma.boolean("label", {
+    true: figma.string("labelText"),
+  }),
+  labelOrientation: figma.enum<QdsProgressLabelOrientation>("labelPosition", {
+    side: "side",
+  }),
+  size: figma.enum<QdsProgressSize>("size", {
+    lg: "lg",
+    sm: "sm",
+  }),
+  valueText: figma.boolean("percentage", {
+    false: undefined,
+    true: "25%",
+  }),
+}
+
+// Determinate progress (indeterminate=False)
+figma.connect(Progress, "<FIGMA_COMPONENTS_BASE>?node-id=4402-120", {
+  example: (props) => <Progress value={25} {...props} />,
+  props: sharedProps,
+  variant: {indeterminate: "False"},
+})
+
+// Indeterminate progress (indeterminate=True)
+figma.connect(Progress, "<FIGMA_COMPONENTS_BASE>?node-id=4402-120", {
+  example: ({valueText, ...props}) => <Progress {...props} />,
+  props: sharedProps,
+  variant: {indeterminate: "True"},
+})
