aca29eb7901e368a1ab99cc71ae2074bdecb13e8
Olaf Kappes <okappes@qti.qualcomm.com>
2026-01-15 16:00:44 +0100

    feat: popover
    Signed-off-by: Olaf Kappes <okappes@qti.qualcomm.com>

---
+++ packages/frameworks/react/src/popover/figma/popover.figma.tsx
@@ -0,0 +1,48 @@
+import figma from "@figma/code-connect"
+
+import {Button} from "@qualcomm-ui/react/button"
+import {Popover} from "@qualcomm-ui/react/popover"
+
+const FIGMA_URL = "<FIGMA_COMPONENTS_BASE>?node-id=5951-1058"
+
+figma.connect(Popover, FIGMA_URL, {
+  example: ({actionItems, content, positioning}) => (
+    <Popover
+      description={content.body}
+      label={content.label}
+      positioning={positioning.placement}
+      trigger={<Button emphasis="primary">Show Popover</Button>}
+    >
+      {actionItems.buttons}
+    </Popover>
+  ),
+  props: {
+    actionItems: figma.nestedProps("_Popover content", {
+      buttons: figma.boolean("actionItems", {
+        true: <Button emphasis="neutral" variant="outline">Button</Button><Button emphasis="primary">Button</Button>,
+      }),
+    }),
+    content: figma.nestedProps("_Popover content", {
+      body: figma.string("body"),
+      label: figma.boolean("heading", {
+        true: figma.string("headingText"),
+      }),
+    }),
+    positioning: figma.nestedProps("_Popover pointer placement", {
+      placement: figma.enum("position", {
+        "bottom-left": {placement: "bottom-start"},
+        "bottom-middle": {placement: "bottom"},
+        "bottom-right": {placement: "bottom-end"},
+        "left-bottom": {placement: "left-end"},
+        "left-middle": {placement: "left"},
+        "left-top": {placement: "left-start"},
+        "right-bottom": {placement: "right-end"},
+        "right-middle": {placement: "right"},
+        "right-top": {placement: "right-start"},
+        "top-left": {placement: "top-start"},
+        "top-middle": {placement: "top"},
+        "top-right": {placement: "top-end"},
+      }),
+    }),
+  },
+})
