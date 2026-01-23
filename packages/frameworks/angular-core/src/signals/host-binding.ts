ba6f6b2ee19e353c50c935e188c53e87c8554840
Ryan Bower <rbower@qti.qualcomm.com>
2026-01-23 00:16:39 -0800

    refactor: replace Function type usage
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/angular-core/src/signals/host-binding.ts
+++ packages/frameworks/angular-core/src/signals/host-binding.ts
@@ -37,7 +37,7 @@ import {
  * ```
  */
 export function assertInjector<Runner extends () => any>(
-  fn: Function,
+  fn: (...args: unknown[]) => any,
   injector: Injector | undefined | null,
   runner: Runner,
 ): ReturnType<Runner>
@@ -53,11 +53,11 @@ export function assertInjector<Runner extends () => any>(
  * @returns Injector
  */
 export function assertInjector(
-  fn: Function,
+  fn: (...args: any[]) => unknown,
   injector: Injector | undefined | null,
 ): Injector
 export function assertInjector(
-  fn: Function,
+  fn: (...args: unknown[]) => unknown,
   injector: Injector | undefined | null,
   runner?: () => any,
 ) {
