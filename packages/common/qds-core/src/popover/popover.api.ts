948929015f630b97c76d0c70df29451e389d0db5
Olaf Kappes <okappes@qti.qualcomm.com>
2026-01-23 11:14:11 +0100

    feat(popover): add brand emphasis
    Signed-off-by: Olaf Kappes <okappes@qti.qualcomm.com>

---
+++ packages/common/qds-core/src/popover/popover.api.ts
@@ -0,0 +1,37 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+import type {Explicit} from "@qualcomm-ui/utils/guard"
+import type {PropNormalizer} from "@qualcomm-ui/utils/machine"
+
+import {popoverClasses} from "./popover.classes"
+import type {
+  QdsPopoverApi,
+  QdsPopoverApiProps,
+  QdsPopoverArrowBindings,
+  QdsPopoverContentBindings,
+} from "./popover.types"
+
+export function createQdsPopoverApi(
+  props: Explicit<QdsPopoverApiProps>,
+  normalize: PropNormalizer,
+): QdsPopoverApi {
+  const emphasis = props.emphasis || "neutral"
+
+  return {
+    emphasis,
+
+    getArrowBindings(): QdsPopoverArrowBindings {
+      return normalize.element({
+        className: popoverClasses.arrow,
+        "data-emphasis": emphasis,
+      })
+    },
+    getContentBindings(): QdsPopoverContentBindings {
+      return normalize.element({
+        className: popoverClasses.content,
+        "data-emphasis": emphasis,
+      })
+    },
+  }
+}
