dbac12c7046ae06972cd88def5f86c490db18d52
Ryan Bower <rbower@qti.qualcomm.com>
2026-01-29 21:48:10 -0800

    fix(json-model-viewer): hide description button for all object types
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/frameworks/react-swagger/src/internal/json-model-viewer/components/data-key-pair.tsx
+++ packages/frameworks/react-swagger/src/internal/json-model-viewer/components/data-key-pair.tsx
@@ -22,7 +22,12 @@ import {useJsonViewerStore, useTypeComponents} from "../stores"
 import type {DataItemProps} from "../type"
 import {getValueSize} from "../utils"
 
-import {getRefName, isReferenceArray, isReferenceObject} from "./internal"
+import {
+  getRefName,
+  isObject,
+  isReferenceArray,
+  isReferenceObject,
+} from "./internal"
 
 export type DataKeyPairProps = {
   className?: string
@@ -249,9 +254,7 @@ export function DataKeyPair(props: DataKeyPairProps) {
         </span>
 
         {Component ? (
-          description &&
-          !isReferenceObject(value) &&
-          !isReferenceArray(value) ? (
+          description && !isObject(value) && !isReferenceArray(value) ? (
             <button
               className="data-key-description-expand-button"
               onClick={() => setShowDescription(!showDescription)}
