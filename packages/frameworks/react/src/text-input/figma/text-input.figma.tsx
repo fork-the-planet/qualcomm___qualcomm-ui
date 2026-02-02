1d4c87e165e836a5a69c3f554e5a0110a50ec031
Olaf Kappes <okappes@qti.qualcomm.com>
2026-02-02 16:28:02 +0100

    fix: text-input
    Signed-off-by: Olaf Kappes <okappes@qti.qualcomm.com>

--- packages/frameworks/react/src/text-input/figma/text-input.figma.tsx
+++ packages/frameworks/react/src/text-input/figma/text-input.figma.tsx
@@ -1,35 +1,43 @@
 // Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
-// @ts-nocheck
 import figma from "@figma/code-connect"
 
 import {TextInput} from "@qualcomm-ui/react/text-input"
 
 const sharedProps = {
-  defaultInputValue: figma.boolean("filled", {
+  defaultValue: figma.boolean("filled", {
     true: figma.string("inputText"),
   }),
   disabled: figma.enum("state", {
     disabled: true,
   }),
   errorText: figma.string("errorText"),
+  hint: figma.boolean("hint", {
+    true: figma.string("hintText"),
+  }),
   invalid: figma.enum("state", {
     invalid: true,
+    "invalid-focus": true,
   }),
   label: figma.boolean("label", {
     true: figma.string("labelText"),
   }),
+  placeholder: figma.string("holderText"),
+  readOnly: figma.enum("state", {
+    "read-only": true,
+  }),
   required: figma.boolean("required"),
   size: figma.enum("size", {
     lg: "lg",
-    md: "md",
     sm: "sm",
   }),
 }
 
 figma.connect(TextInput, "<FIGMA_COMPONENTS_BASE>?node-id=4227-2418", {
-  example: ({label, ...props}) => <TextInput label={label} {...props} />,
+  example: ({hint, label, ...props}) => (
+    <TextInput hint={hint} label={label} {...props} />
+  ),
   props: {
     ...sharedProps,
     endIcon: figma.instance("iconRxs"),
@@ -38,7 +46,9 @@ figma.connect(TextInput, "<FIGMA_COMPONENTS_BASE>?node-id=4227-2418", {
 })
 
 figma.connect(TextInput, "<FIGMA_COMPONENTS_BASE>?node-id=4227-2418", {
-  example: ({label, ...props}) => <TextInput label={label} {...props} />,
+  example: ({hint, label, ...props}) => (
+    <TextInput hint={hint} label={label} {...props} />
+  ),
   props: {
     ...sharedProps,
     endIcon: figma.instance("iconRsm"),
