f78cfe59a7929f8ceaa32d7d05a0640c19d5daca
Ryan Bower <rbower@qti.qualcomm.com>
2026-02-14 23:46:56 -0800

    add license header
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/mdx-vite/src/ai-knowledge/generator/filter-text-directives.ts
+++ packages/common/mdx-vite/src/ai-knowledge/generator/filter-text-directives.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 import type {Text} from "mdast"
 import type {Plugin} from "unified"
 import {visit} from "unist-util-visit"
