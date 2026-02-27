d281c23b5fe6076a43239be15a0da9722a103a41
Ryan Bower <rbower@qti.qualcomm.com>
2026-02-27 00:02:46 -0800

    feat(react-core): add useNoMemo compat utility for react compiler opt-out
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

---
+++ packages/frameworks/react-core/src/compat/use-no-memo.ts
@@ -0,0 +1,12 @@
+/**
+ * Calls the provided factory function and returns its result without memoization.
+ * Uses the `"use no memo"` directive to opt out of React's automatic memoization
+ * (e.g., React Compiler).
+ *
+ * @param factory - A function that produces the value to return.
+ * @returns The result of invoking the factory function.
+ */
+export function useNoMemo<const T>(factory: () => T): T {
+  "use no memo"
+  return factory()
+}
