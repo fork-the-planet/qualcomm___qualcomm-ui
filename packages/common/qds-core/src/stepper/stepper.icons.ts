a439534a079ea753ed9c47b57b79efdaecbb9e57
Ryan Bower <rbower@qti.qualcomm.com>
2026-04-07 21:34:35 -0700

    feat(stepper): add QDS stepper styles and types
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

---
+++ packages/common/qds-core/src/stepper/stepper.icons.ts
@@ -0,0 +1,23 @@
+type HtmlAttributes = {
+  [key: string]: string | number
+}
+type LucideIconNode = readonly [string, HtmlAttributes]
+type LucideIconData = readonly LucideIconNode[]
+
+export const StepperIndicatorAlert: LucideIconData = [
+  [
+    "line",
+    {key: "1pkeuh", "stroke-width": "3", x1: "12", x2: "12", y1: "6", y2: "13"},
+  ],
+  [
+    "line",
+    {
+      key: "4dfq90",
+      "stroke-width": "3",
+      x1: "12",
+      x2: "12.01",
+      y1: "18",
+      y2: "18",
+    },
+  ],
+]
