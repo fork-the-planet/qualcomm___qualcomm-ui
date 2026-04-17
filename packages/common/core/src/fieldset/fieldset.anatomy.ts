69bfdd56d3a143e6822df74383abc67ebfed4312
Ryan Bower <rbower@qti.qualcomm.com>
2026-04-17 11:30:33 -0700

    refactor(fieldset): update anatomy attributes
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

---
+++ packages/common/core/src/fieldset/fieldset.anatomy.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+import {type Anatomy, createAnatomy} from "@qualcomm-ui/utils/anatomy"
+
+const parts = ["root", "legend", "hint", "errorText"] as const
+
+export const fieldsetAnatomy: Anatomy<"fieldset", (typeof parts)[number]> =
+  createAnatomy("fieldset").parts(...parts)
