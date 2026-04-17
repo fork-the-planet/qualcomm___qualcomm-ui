e75e2952ecd80106e5b8245066859afe1e282c01
Ryan Bower <rbower@qti.qualcomm.com>
2026-04-17 11:39:01 -0700

    refactor(password-input): update anatomy attributes
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

---
+++ packages/common/core/src/password-input/password-input.anatomy.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+import {type Anatomy, createAnatomy} from "@qualcomm-ui/utils/anatomy"
+
+const parts = [
+  "root",
+  "label",
+  "input",
+  "inputGroup",
+  "clearTrigger",
+  "errorIndicator",
+  "errorText",
+  "hint",
+  "visibilityTrigger",
+] as const
+
+export const passwordInputAnatomy: Anatomy<
+  "passwordInput",
+  (typeof parts)[number]
+> = createAnatomy("passwordInput").parts(...parts)
