701c817f4a4a2bb1fcb1c856c83cbe9bc31c1499
Ryan Bower <rbower@qti.qualcomm.com>
2026-04-19 16:07:57 -0700

    docs: clean up menu and dialog demos
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/docs/angular-docs/src/routes/components+/menu+/demos/menu-within-dialog-demo.ts
+++ packages/docs/angular-docs/src/routes/components+/menu+/demos/menu-within-dialog-demo.ts
@@ -6,7 +6,7 @@ import {DialogModule} from "@qualcomm-ui/angular/dialog"
 import {IconDirective} from "@qualcomm-ui/angular/icon"
 import {MenuModule} from "@qualcomm-ui/angular/menu"
 import {provideIcons} from "@qualcomm-ui/angular-core/lucide"
-import {PortalComponent} from "@qualcomm-ui/angular-core/portal"
+import {PortalDirective} from "@qualcomm-ui/angular-core/portal"
 
 @Component({
   imports: [
@@ -14,7 +14,7 @@ import {PortalComponent} from "@qualcomm-ui/angular-core/portal"
     ButtonModule,
     DialogModule,
     IconDirective,
-    PortalComponent,
+    PortalDirective,
   ],
   providers: [provideIcons({X})],
   selector: "menu-within-dialog-demo",
@@ -24,38 +24,39 @@ import {PortalComponent} from "@qualcomm-ui/angular-core/portal"
         Show Dialog
       </button>
 
-      <q-portal>
+      <ng-container *qPortal>
         <div q-dialog-backdrop></div>
         <div q-dialog-positioner>
           <section class="w-72" q-dialog-content>
-            <h3 q-dialog-heading>Dialog Title</h3>
-            <q-menu>
-              <button
-                class="place-self-start"
-                emphasis="primary"
-                q-menu-button
-                variant="fill"
-              >
-                Show Menu
-              </button>
+            <div q-dialog-body>
+              <button q-dialog-close-button></button>
+              <h3 q-dialog-heading>Dialog Title</h3>
+              <q-menu>
+                <button
+                  class="place-self-start"
+                  emphasis="primary"
+                  q-menu-button
+                  variant="fill"
+                >
+                  Show Menu
+                </button>
 
-              <div q-menu-positioner>
-                <div q-menu-content>
-                  <button q-menu-item value="new-text-file">
-                    New Text File
-                  </button>
-                  <button q-menu-item value="new-file">New File...</button>
-                  <button q-menu-item value="open-file">Open File...</button>
-                  <button q-menu-item value="export">Export</button>
+                <div q-menu-positioner>
+                  <div q-menu-content>
+                    <button q-menu-item value="new-text-file">
+                      New Text File
+                    </button>
+                    <button q-menu-item value="new-file">New File...</button>
+                    <button q-menu-item value="open-file">Open File...</button>
+                    <button q-menu-item value="export">Export</button>
+                  </div>
                 </div>
-              </div>
-            </q-menu>
-            <button q-dialog-close-trigger>
-              <svg qIcon="X"></svg>
-            </button>
+              </q-menu>
+              
+            </div>
           </section>
         </div>
-      </q-portal>
+      </ng-container>
     </div>
   `,
 })
