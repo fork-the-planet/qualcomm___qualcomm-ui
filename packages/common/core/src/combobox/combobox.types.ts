cfaa97bff45e735c151260d41d52f2fb8610136c
Ryan Bower <rbower@qti.qualcomm.com>
2026-05-22 00:08:56 -0700

    feat(combobox): support aria labels
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/common/core/src/combobox/combobox.types.ts
+++ packages/common/core/src/combobox/combobox.types.ts
@@ -817,6 +817,10 @@ export interface ComboboxInputBindings extends Part<"input"> {
   "aria-controls": string
   "aria-expanded": BooleanAriaAttr
   "aria-invalid": BooleanAriaAttr
+  /**
+   * @since next-release
+   */
+  "aria-labelledby": string | undefined
   autoCapitalize: "none"
   autoComplete: "off"
   autoCorrect: "off"
