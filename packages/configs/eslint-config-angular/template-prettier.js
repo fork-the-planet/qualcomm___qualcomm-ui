62909042386a56ae7ffd97a059b2fa3f50a2802a
Ryan Bower <rbower@qti.qualcomm.com>
2026-06-09 22:07:23 -0700

    refactor!: consolidate angular configs
    Only two configs are exported now: typescriptRecommended and templateRecommended
    
    Signed-off-by: Ryan Bower <rbower@qti.qualcomm.com>

--- packages/configs/eslint-config-angular/template-prettier.js
+++
@@ -1,9 +0,0 @@
-import oxfmtPlugin from "eslint-plugin-oxfmt"
-import {defineConfig} from "eslint/config"
-
-export default defineConfig({
-  extends: [oxfmtPlugin.configs.recommendedWithoutParser],
-  rules: {
-    "oxfmt/oxfmt": "error",
-  },
-})
